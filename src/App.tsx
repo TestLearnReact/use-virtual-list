a9160964dd09163150628cd0f41242cf90cfd8c1
TestLearnReact <socialdev@gmx.de>
2023-04-03 19:26:30 +0200

    new config eslint/prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
@@ -9,7 +9,11 @@ function App() {
 	return (
 		<div className="App">
 			<div>
-				<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+				<a
+					href="https://vitej--------------------------------------------------------------------------------s.dev"
+					target="_blank"
+					rel="noreferrer"
+				>
 					<img src={viteLogo} className="logo" alt="Vite logo" />
 				</a>
 				<a href="https://reactjs.org" target="_blank" rel="noreferrer">
