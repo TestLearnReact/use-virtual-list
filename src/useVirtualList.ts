4601739723f3f58cf43116ebbe68dc5efbff4951
TestLearnReact <socialdev@gmx.de>
2023-04-06 10:59:03 +0200

    delete useResolvedElement() ,delte return useMemo()

--- src/useVirtualList.ts
+++ src/useVirtualList.ts
@@ -1,4 +1,3 @@
-//import { useCallback, useMemo, useRef, useState } from 'react';
 import {
 	RefCallback,
 	RefObject,
@@ -27,8 +26,6 @@ import {
 	getExtendedVisibleItemRange,
 	isSameRange,
 	needNewCalcVisbleRange,
-
-	//useResolvedElement,
 } from './utils';
 
 export function useVirtualList<
@@ -240,35 +237,7 @@ export function useVirtualList<
 		if (xouterRef) refOuterContainer.current = xouterRef.current;
 	}, [xouterRef, xinnerRef]);
 
-	// /**
-	//  * prevent rerender / use useRef or ref={refOuter} in parent component
-	//  * */
-	// const refCallbackOuter = useResolvedElement<O>(
-	// 	useCallback((element) => {
-	// 		console.log('outer: ', element.getBoundingClientRect());
-	// 		refOuterContainer.current = element;
-	// 	}, []),
-	// 	xouterRef
-	// );
-
-	// /**
-	//  * prevent rerender / use useRef or ref={refInner} in parent component
-	//  * */
-	// const refCallbackInner = useResolvedElement<I>(
-	// 	useCallback(
-	// 		(element) => {
-	// 			console.log('inner: ', element.getBoundingClientRect());
-	// 			// setRerender(!rerender);
-	// 			refInnerContainer.current = element;
-	// 		},
-	// 		[refInnerContainer.current?.offsetHeight]
-	// 	),
-	// 	xinnerRef
-	// );
-
 	return {
-		// refOuter: useMemo(() => refCallbackOuter, [refCallbackOuter]),
-		// refInner: useMemo(() => refCallbackInner, [refCallbackInner]),
 		refOuter: refOuterContainer,
 		refInner: refInnerContainer,
 		visibleItems: hookReturnState.visibleItems, // visibleItems,
@@ -293,85 +262,3 @@ export function useVirtualList<
 // };
 
 // todo declared twice hooks
-
-type SubscriberCleanupFunction = () => void;
-type SubscriberResponse = SubscriberCleanupFunction | void;
-
-// This could've been more streamlined with internal state instead of abusing
-// refs to such extent, but then composing hooks and components could not opt out of unnecessary renders.
-export default function useResolvedElement<T extends Element>(
-	subscriber: (element: T) => SubscriberResponse,
-	refOrElement?: T | RefObject<T> | null
-): RefCallback<T> {
-	const lastReportRef = useRef<{
-		element: T | null;
-		subscriber: typeof subscriber;
-		cleanup?: SubscriberResponse;
-	} | null>(null);
-	const refOrElementRef = useRef<typeof refOrElement>(null);
-	refOrElementRef.current = refOrElement;
-	const cbElementRef = useRef<T | null>(null);
-
-	// Calling re-evaluation after each render without using a dep array,
-	// as the ref object's current value could've changed since the last render.
-	useEffect(() => {
-		evaluateSubscription();
-	});
-
-	const evaluateSubscription = useCallback(() => {
-		const cbElement = cbElementRef.current;
-		const refOrElement = refOrElementRef.current;
-		// console.log(
-		//   cbElement,
-		//   refOrElement,
-		//   refOrElement instanceof Element,
-		//   //refOrElement.current,
-		// );
-		// Ugly ternary. But smaller than an if-else block.
-		const element: T | null = cbElement
-			? cbElement
-			: refOrElement
-			? refOrElement instanceof Element
-				? refOrElement
-				: refOrElement.current
-			: null;
-
-		if (
-			lastReportRef.current &&
-			lastReportRef.current.element === element &&
-			lastReportRef.current.subscriber === subscriber
-		) {
-			return;
-		}
-		// debugger;
-		if (lastReportRef.current && lastReportRef.current.cleanup) {
-			lastReportRef.current.cleanup();
-		}
-		lastReportRef.current = {
-			element,
-			subscriber,
-			// Only calling the subscriber, if there's an actual element to report.
-			// Setting cleanup to undefined unless a subscriber returns one, as an existing cleanup function would've been just called.
-			cleanup: element ? subscriber(element) : undefined,
-		};
-	}, [subscriber]);
-
-	// making sure we call the cleanup function on unmount
-	useEffect(() => {
-		return () => {
-			if (lastReportRef.current && lastReportRef.current.cleanup) {
-				lastReportRef.current.cleanup();
-				lastReportRef.current = null;
-			}
-		};
-	}, []);
-
-	return useCallback(
-		(element) => {
-			cbElementRef.current = element;
-			evaluateSubscription();
-		},
-		[evaluateSubscription]
-	);
-}
-export { useResolvedElement };
