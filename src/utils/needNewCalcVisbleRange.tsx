7abc5717f5cde6587d2e67ece415a6299320f0a6
TestLearnReact <socialdev@gmx.de>
2023-04-12 17:02:30 +0200

    delete scrollOffsetX/Y add containerStyles

--- src/utils/needNewCalcVisbleRange.tsx
+++ src/utils/needNewCalcVisbleRange.tsx
@@ -1,6 +1,6 @@
 import { MutableRefObject } from 'react';
 import { Direction, Measure } from '../types';
-import { TCacheValues } from '../hooks';
+import { IReturnContainerStyles, TCacheValues } from '../hooks';
 // import { TCacheValues } from '../../hooks/custom-hooks/use-cache/types';
 // import { Measure } from '../../types';
 
@@ -11,30 +11,40 @@ export function needNewCalcVisbleRange<
 	msDataRef,
 	cache,
 	///refInnerContainer,
-	refOuterContainer,
-	totalSize,
+	///refOuterContainer,
+	///totalSize,
 	useWindowScroll,
 	listDirection,
+	containerStyles,
 }: {
 	msDataRef: MutableRefObject<Measure[]>;
 	cache: TCacheValues;
 	///refInnerContainer: React.MutableRefObject<I | undefined>; // todo check type / size as parameter
-	refOuterContainer: React.RefObject<HTMLElement | undefined>; //React.MutableRefObject<O | undefined>; // todo check type / size as parameter
-	totalSize: number;
+	///refOuterContainer: React.RefObject<HTMLElement | undefined>; //React.MutableRefObject<O | undefined>; // todo check type / size as parameter
+	///totalSize: number;
 	useWindowScroll: boolean;
 	listDirection: Direction;
+	containerStyles: IReturnContainerStyles['containerStyles'];
 }) {
-	// direction / overscan
-	// const sizeStyleProp =
-	//   listDirection === Direction.Vertical ? 'height' : 'width';
-	//debugger;
 	const {
 		visibleItemRange,
 		scrollData: { scrollForward, scrollOffsetY, scrollOffsetX },
 	} = cache;
 
-	const scrollOffset =
-		listDirection === Direction.Vertical ? scrollOffsetY : scrollOffsetX;
+	const {
+		outerContainerStyle: { height: listHeight, width: listWidth, left, top },
+		innerContainerStyle: { totalSize },
+	} = containerStyles;
+
+	let scrollOffset = scrollOffsetY;
+	let listSize = listHeight;
+	let topLeftpos = top;
+
+	if (listDirection === Direction.Horizontal) {
+		scrollOffset = scrollOffsetX;
+		listSize = listWidth;
+		topLeftpos = left;
+	}
 
 	const first = msDataRef.current[visibleItemRange[0]];
 	const last =
@@ -43,46 +53,24 @@ export function needNewCalcVisbleRange<
 	const veryFirst = msDataRef.current[0];
 	const veryLast = msDataRef.current[msDataRef.current.length - 1];
 
-	const listHeight = refOuterContainer.current?.offsetHeight || 0;
-	const listWidth = refOuterContainer.current?.offsetWidth || 0;
-
-	const listSize =
-		listDirection === Direction.Vertical ? listHeight : listWidth;
-
-	let rect = { top: 0, left: 0 }; // todo getBoundingClientRect() outer wrapper x,y scroll
-	if (useWindowScroll) rect = { top: 60, left: 0 };
-
-	// if (scrollOffsetY < rect.top) {
-	// 	return false;
-	// }
-	if (scrollOffset < rect.top) {
+	if (useWindowScroll && scrollOffset < topLeftpos) {
 		return false;
 	}
 
-	// let needNewCalc = false;
 	let needNewCalc = true;
-
-	// if (scrollOffset >= 750) debugger;
 
 	// scroll down
 	if (scrollForward) {
 		// if (last.idx == veryLast.idx) return false;
-
 		switch (true) {
 			// last visible item changed
-			//			case scrollOffsetY + listHeight > last.start && last.idx !== veryLast.idx:
 			case scrollOffset + listSize > last.start && last.idx !== veryLast.idx:
-				//debugger;
 				break;
 			// first visible item changed
-			//case scrollOffsetY - first.size > first.end:
 			case scrollOffset - first.size > first.end:
-				//debugger;
 				break;
 			// edge case, container size bigger than totalsize
-			//case scrollOffsetY > totalSize:
 			case scrollOffset > totalSize:
-				//debugger;
 				break;
 			default:
 				needNewCalc = false;
@@ -93,14 +81,11 @@ export function needNewCalcVisbleRange<
 	// scroll up
 	if (!scrollForward) {
 		//if (first.idx == veryFirst.idx) return false;
-
 		switch (true) {
 			// first visible item changed
-			//			case scrollOffsetY < first.end && first.idx !== veryFirst.idx:
 			case scrollOffset < first.end && first.idx !== veryFirst.idx:
 				break;
 			// last visible item changed
-			//case scrollOffsetY + listHeight < last.start - last.size:
 			case scrollOffset + listSize < last.start - last.size:
 				break;
 			default:
