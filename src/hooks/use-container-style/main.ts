b4221cd0976ad38f80a1cfec9e9648e9df686e0c
TestLearnReact <socialdev@gmx.de>
2023-04-12 12:42:59 +0200

    add top, left

--- src/hooks/use-container-style/main.ts
+++ src/hooks/use-container-style/main.ts
@@ -3,6 +3,16 @@ import { TCacheValues } from '../use-cache/types';
 import { Measure } from '../../types';
 import { getFirstArrItem, getLastArrItem } from '../../utils';
 
+// todo no sense refInner/Outer? maybe for neednewcalc() x/y scroll
+
+interface IProps<O, I> {
+	msDataRef: React.MutableRefObject<Measure[]>;
+	cache: TCacheValues;
+	itemsLength: number;
+	refOuterContainer: React.RefObject<O>; //React.MutableRefObject<O | undefined>;
+	refInnerContainer: React.RefObject<I>; //React.MutableRefObject<I | undefined>;
+}
+
 export const useContainerStyle = <
 	O extends HTMLElement = HTMLElement,
 	I extends HTMLElement = O
@@ -12,120 +22,13 @@ export const useContainerStyle = <
 	itemsLength,
 	refOuterContainer,
 	refInnerContainer,
-}: {
-	msDataRef: React.MutableRefObject<Measure[]>;
-	cache: TCacheValues;
-	itemsLength: number;
-	refOuterContainer: React.RefObject<O>; //React.MutableRefObject<O | undefined>;
-	refInnerContainer: React.RefObject<I>; //React.MutableRefObject<I | undefined>;
-}) => {
-	// const [stateStyle, setStateStyle] = useState<{
-	//   outerContainerStyle: {
-	//     height: number;
-	//     width: number;
-	//   };
-	//   innerContainerStyle: {
-	//     innerMargin: number;
-	//     innerSize: number;
-	//     totalSize: number;
-	//     heightInner: number;
-	//     widthInner: number;
-	//   };
-	// }>({
-	//   outerContainerStyle: {
-	//     height: 0,
-	//     width: 0,
-	//   },
-	//   innerContainerStyle: {
-	//     innerMargin: 0,
-	//     innerSize: 0,
-	//     totalSize: 0,
-	//     heightInner: 0,
-	//     widthInner: 0,
-	//   },
-	// });
-
-	// const containerStyles = useMemo<{
-	//   innerMargin: number;
-	//   innerSize: number;
-	//   totalSize: number;
-	//   height: number;
-	//   width: number;
-	//   heightInner: number;
-	//   widthInner: number;
-	// }>(() => {
-	//   let innerMargin = 0;
-	//   let totalSize = 0;
-	//   let innerSize = 0;
-	//   let height = refOuterContainer.current?.offsetHeight || 0;
-	//   let width = refOuterContainer.current?.offsetWidth || 0;
-	//   let heightInner = refInnerContainer.current?.offsetHeight || 0;
-	//   let widthInner = refInnerContainer.current?.offsetWidth || 0;
-
-	//   console.log('heightInner,widthInner', heightInner, widthInner);
-
-	//   let lastVisibleItemOffsets =
-	//     msDataRef.current[getLastArrItem(cache.visibleItemRange)];
-
-	//   if (lastVisibleItemOffsets) {
-	//     innerMargin =
-	//       msDataRef.current[getFirstArrItem(cache.visibleItemRange)]?.start || 0;
-
-	//     totalSize = Math[
-	//       getLastArrItem(cache.visibleItemRange) < itemsLength ? 'max' : 'min'
-	//     ](
-	//       lastVisibleItemOffsets.end, //+ lastVisibleItemOffsets.size, // todo
-	//       getLastArrItem(msDataRef.current).end,
-	//     );
-	//   }
-	//   innerSize = totalSize - innerMargin;
-
-	//   setStateStyle({
-	//     outerContainerStyle: { height, width },
-	//     innerContainerStyle: {
-	//       innerMargin,
-	//       innerSize,
-	//       totalSize,
-	//       heightInner,
-	//       widthInner,
-	//     },
-	//   });
-
-	//   return {
-	//     innerMargin,
-	//     innerSize,
-	//     totalSize,
-	//     height,
-	//     width,
-	//     heightInner,
-	//     widthInner,
-	//   };
-	// }, [
-	//   msDataRef.current,
-	//   cache.visibleItemRange,
-	//   // refOuterContainer.current,
-	//   // refInnerContainer.current?.offsetWidth,
-	//   // refInnerContainer.current?.offsetHeight,
-	// ]);
-
-	// return {
-	//   outerContainerStyle: {
-	//     height: stateStyle.outerContainerStyle.height,
-	//     width: stateStyle.outerContainerStyle.width,
-	//   },
-	//   innerContainerStyle: {
-	//     innerMargin: stateStyle.innerContainerStyle.innerMargin,
-	//     totalSize: stateStyle.innerContainerStyle.totalSize,
-	//     innerSize: stateStyle.innerContainerStyle.innerSize,
-	//     heightInner: stateStyle.innerContainerStyle.heightInner,
-	//     widthInner: stateStyle.innerContainerStyle.widthInner,
-	//   },
-	// };
-
+}: IProps<O, I>) => {
 	const containerStyles = useMemo<{
 		innerMargin: number;
 		innerSize: number;
 		totalSize: number;
+		top: number;
+		left: number;
 		height: number;
 		width: number;
 		heightInner: number;
@@ -134,12 +37,19 @@ export const useContainerStyle = <
 		let innerMargin = 0;
 		let totalSize = 0;
 		let innerSize = 0;
+		let top = 0;
+		let left = 0;
 		const height = refOuterContainer.current?.offsetHeight || 0;
 		const width = refOuterContainer.current?.offsetWidth || 0;
 		const heightInner = refInnerContainer.current?.offsetHeight || 0;
 		const widthInner = refInnerContainer.current?.offsetWidth || 0;
 
-		//console.log('heightInner,widthInner', heightInner, widthInner);
+		if (refOuterContainer && refOuterContainer.current) {
+			const { top: t, left: l } =
+				refOuterContainer.current.getBoundingClientRect();
+			top = t;
+			left = l;
+		}
 
 		const lastVisibleItemOffsets =
 			msDataRef.current[getLastArrItem(cache.visibleItemRange)];
@@ -161,23 +71,27 @@ export const useContainerStyle = <
 			innerMargin,
 			innerSize,
 			totalSize,
+			top,
+			left,
 			height,
 			width,
 			heightInner,
 			widthInner,
 		};
 	}, [
-		msDataRef.current,
 		cache.visibleItemRange,
-		refOuterContainer.current,
-		refInnerContainer.current?.offsetWidth,
-		refInnerContainer.current?.offsetHeight,
+		itemsLength,
+		msDataRef,
+		refInnerContainer,
+		refOuterContainer,
 	]);
 
 	return {
 		outerContainerStyle: {
 			height: containerStyles.height,
 			width: containerStyles.width,
+			top: containerStyles.top,
+			left: containerStyles.left,
 		},
 		innerContainerStyle: {
 			innerMargin: containerStyles.innerMargin,
@@ -300,4 +214,107 @@ export const useContainerStyle = <
 // return {
 //   outerContainerStyle: containerStyle.outerContainerStyle,
 //   innerContainerStyle: containerStyle.innerContainerStyle,
+// };
+
+// const [stateStyle, setStateStyle] = useState<{
+//   outerContainerStyle: {
+//     height: number;
+//     width: number;
+//   };
+//   innerContainerStyle: {
+//     innerMargin: number;
+//     innerSize: number;
+//     totalSize: number;
+//     heightInner: number;
+//     widthInner: number;
+//   };
+// }>({
+//   outerContainerStyle: {
+//     height: 0,
+//     width: 0,
+//   },
+//   innerContainerStyle: {
+//     innerMargin: 0,
+//     innerSize: 0,
+//     totalSize: 0,
+//     heightInner: 0,
+//     widthInner: 0,
+//   },
+// });
+
+// const containerStyles = useMemo<{
+//   innerMargin: number;
+//   innerSize: number;
+//   totalSize: number;
+//   height: number;
+//   width: number;
+//   heightInner: number;
+//   widthInner: number;
+// }>(() => {
+//   let innerMargin = 0;
+//   let totalSize = 0;
+//   let innerSize = 0;
+//   let height = refOuterContainer.current?.offsetHeight || 0;
+//   let width = refOuterContainer.current?.offsetWidth || 0;
+//   let heightInner = refInnerContainer.current?.offsetHeight || 0;
+//   let widthInner = refInnerContainer.current?.offsetWidth || 0;
+
+//   console.log('heightInner,widthInner', heightInner, widthInner);
+
+//   let lastVisibleItemOffsets =
+//     msDataRef.current[getLastArrItem(cache.visibleItemRange)];
+
+//   if (lastVisibleItemOffsets) {
+//     innerMargin =
+//       msDataRef.current[getFirstArrItem(cache.visibleItemRange)]?.start || 0;
+
+//     totalSize = Math[
+//       getLastArrItem(cache.visibleItemRange) < itemsLength ? 'max' : 'min'
+//     ](
+//       lastVisibleItemOffsets.end, //+ lastVisibleItemOffsets.size, // todo
+//       getLastArrItem(msDataRef.current).end,
+//     );
+//   }
+//   innerSize = totalSize - innerMargin;
+
+//   setStateStyle({
+//     outerContainerStyle: { height, width },
+//     innerContainerStyle: {
+//       innerMargin,
+//       innerSize,
+//       totalSize,
+//       heightInner,
+//       widthInner,
+//     },
+//   });
+
+//   return {
+//     innerMargin,
+//     innerSize,
+//     totalSize,
+//     height,
+//     width,
+//     heightInner,
+//     widthInner,
+//   };
+// }, [
+//   msDataRef.current,
+//   cache.visibleItemRange,
+//   // refOuterContainer.current,
+//   // refInnerContainer.current?.offsetWidth,
+//   // refInnerContainer.current?.offsetHeight,
+// ]);
+
+// return {
+//   outerContainerStyle: {
+//     height: stateStyle.outerContainerStyle.height,
+//     width: stateStyle.outerContainerStyle.width,
+//   },
+//   innerContainerStyle: {
+//     innerMargin: stateStyle.innerContainerStyle.innerMargin,
+//     totalSize: stateStyle.innerContainerStyle.totalSize,
+//     innerSize: stateStyle.innerContainerStyle.innerSize,
+//     heightInner: stateStyle.innerContainerStyle.heightInner,
+//     widthInner: stateStyle.innerContainerStyle.widthInner,
+//   },
 // };
