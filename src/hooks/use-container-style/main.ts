147f37e327ac6b349129a9a8e5659d111d82d395
TestLearnReact <socialdev@gmx.de>
2023-04-15 16:19:45 +0200

    useRef, useEffect instead of useMemo, useState

--- src/hooks/use-container-style/main.ts
+++ src/hooks/use-container-style/main.ts
@@ -1,10 +1,17 @@
-import { useEffect, useMemo, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { getFirstArrItem, getLastArrItem } from '../../utils';
 import { IProps, IReturnContainerStyles } from './types';
 
-// todo no sense refInner/Outer? maybe for neednewcalc() x/y scroll
+type IDomRect = Omit<DOMRect, 'y' | 'x' | 'toJSON'>;
 
-const init = { height: 0, width: 0, top: 0, left: 0 };
+const initDomRect: IDomRect = {
+	height: 0,
+	width: 0,
+	top: 0,
+	bottom: 0,
+	left: 0,
+	right: 0,
+};
 
 export const useContainerStyle = <
 	O extends HTMLElement = HTMLElement,
@@ -16,128 +23,32 @@ export const useContainerStyle = <
 	refOuterContainer,
 	refInnerContainer,
 }: IProps<O, I>): IReturnContainerStyles => {
-	const {
-		height: oHeight,
-		width: oWidth,
-		top: oTop,
-		left: oLeft,
-	} = refOuterContainer.current?.getBoundingClientRect() || init;
-	const { height: iHeight, width: iWidth } =
-		refInnerContainer.current?.getBoundingClientRect() || init;
+	const refOuterState = useRef(initDomRect);
 
-	// function impressionTracker({
-	// 	msDataRef,
-	// 	cache,
-	// 	itemsLength,
-	// 	refOuterContainer,
-	// 	refInnerContainer,
-	// }: IProps<O, I>) {
-	// 	let innerMargin = 0;
-	// 	let totalSize = 0;
-	// 	let innerSize = 0;
-	// 	let top = 0;
-	// 	let left = 0;
-	// 	const height = refOuterContainer.current?.offsetHeight || 0;
-	// 	const width = refOuterContainer.current?.offsetWidth || 0;
-	// 	const heightInner = refInnerContainer.current?.offsetHeight || 0;
-	// 	const widthInner = refInnerContainer.current?.offsetWidth || 0;
+	const refhookReturn = useRef({
+		outerContainerStyle: refOuterState.current,
+		innerContainerStyle: {
+			innerMargin: 0,
+			totalSize: 0,
+			innerSize: refInnerContainer.current?.offsetHeight || 0,
+			heightInner: -1,
+			widthInner: -1,
+		},
+	});
 
-	// 	if (refOuterContainer && refOuterContainer.current) {
-	// 		const { top: t, left: l } =
-	// 			refOuterContainer.current.getBoundingClientRect();
-	// 		top = t;
-	// 		left = l;
-	// 	}
+	useEffect(() => {
+		if (!refOuterContainer.current) return;
+		refOuterState.current = refOuterContainer.current?.getBoundingClientRect();
+	}, [refOuterContainer]);
 
-	// 	const lastVisibleItemOffsets =
-	// 		msDataRef.current[getLastArrItem(cache.visibleItemRange)];
-
-	// 	if (lastVisibleItemOffsets) {
-	// 		innerMargin =
-	// 			msDataRef.current[getFirstArrItem(cache.visibleItemRange)]?.start || 0;
-
-	// 		totalSize = Math[
-	// 			getLastArrItem(cache.visibleItemRange) < itemsLength ? 'max' : 'min'
-	// 		](
-	// 			lastVisibleItemOffsets.end, //+ lastVisibleItemOffsets.size, // todo
-	// 			getLastArrItem(msDataRef.current).end
-	// 		);
-	// 	}
-	// 	innerSize = totalSize - innerMargin;
-
-	// 	return {
-	// 		outerContainerStyle: {
-	// 			height,
-	// 			width,
-	// 			top,
-	// 			left,
-	// 		},
-	// 		innerContainerStyle: {
-	// 			innerMargin,
-	// 			totalSize,
-	// 			innerSize,
-	// 			heightInner,
-	// 			widthInner,
-	// 		},
-	// 	};
-
-	// 	// console.log(
-	// 	// 	'params',
-	// 	// 	refOuterContainer.current?.offsetHeight,
-	// 	// 	refInnerContainer.current?.offsetHeight
-	// 	// );
-	// 	// return;
-	// }
-
-	// const initialTrackingValues = useRef({
-	// 	tracker: impressionTracker,
-	// 	params: {
-	// 		msDataRef,
-	// 		cache,
-	// 		itemsLength,
-	// 		refOuterContainer,
-	// 		refInnerContainer,
-	// 	},
-	// });
-	// const [containerStyles, setContainerStyles] = useState({
-	// 	outerContainerStyle: {
-	// 		height: 0,
-	// 		width: 0,
-	// 		top: 0,
-	// 		left: 0,
-	// 	},
-	// 	innerContainerStyle: {
-	// 		innerMargin: 0,
-	// 		totalSize: 0,
-	// 		innerSize: 0,
-	// 		heightInner: 0,
-	// 		widthInner: 0,
-	// 	},
-	// });
-
-	// // track impression
-	// useEffect(() => {
-	// 	const { tracker, params } = initialTrackingValues.current;
-	// 	const test = tracker(params);
-	// 	setContainerStyles(test);
-	// }, [h]); // you get NO eslint warnings for tracker or params
-
-	const containerStyles = useMemo(() => {
+	useEffect(() => {
 		let innerMargin = 0;
 		let totalSize = 0;
 		let innerSize = 0;
-		let top = 0;
-		let left = 0;
-		const height = refOuterContainer.current?.offsetHeight || 0;
-		const width = refOuterContainer.current?.offsetWidth || 0;
-		const heightInner = refInnerContainer.current?.offsetHeight || 0;
-		const widthInner = refInnerContainer.current?.offsetWidth || 0;
 
-		if (refOuterContainer && refOuterContainer.current) {
-			const { top: t, left: l } =
-				refOuterContainer.current.getBoundingClientRect();
-			top = t;
-			left = l;
+		// no cache.visibleItemRange are set on init. Dont want render/setState for such simple values
+		if (cache.visibleItemRange[0] == -1) {
+			totalSize = msDataRef.current[msDataRef.current.length - 1].end;
 		}
 
 		const lastVisibleItemOffsets =
@@ -156,261 +67,20 @@ export const useContainerStyle = <
 		}
 		innerSize = totalSize - innerMargin;
 
-		return {
-			outerContainerStyle: {
-				height: oHeight,
-				width,
-				top,
-				left,
-			},
+		refhookReturn.current = {
 			innerContainerStyle: {
 				innerMargin,
 				totalSize,
 				innerSize,
-				heightInner,
-				widthInner,
+				heightInner: -1,
+				widthInner: -1,
 			},
+			outerContainerStyle: refOuterState.current,
 		};
-	}, [
-		cache.visibleItemRange,
-		itemsLength,
-		msDataRef,
-		refInnerContainer,
-		refOuterContainer,
-		//refOuterContainer.current?.offsetHeight,
-		oHeight,
-	]);
+	}, [cache.visibleItemRange, itemsLength, msDataRef]);
 
-	return { containerStyles: containerStyles, measured: 0 };
-	// return {
-	// 	outerContainerStyle: {
-	// 		height: containerStyles.height,
-	// 		width: containerStyles.width,
-	// 		top: containerStyles.top,
-	// 		left: containerStyles.left,
-	// 	},
-	// 	innerContainerStyle: {
-	// 		innerMargin: containerStyles.innerMargin,
-	// 		totalSize: containerStyles.totalSize,
-	// 		innerSize: containerStyles.innerSize,
-	// 		heightInner: containerStyles.heightInner,
-	// 		widthInner: containerStyles.widthInner,
-	// 	},
-	// };
+	return {
+		containerStyles: refhookReturn.current,
+		measured: 0,
+	};
 };
-// const outerContainerStyle = useMemo<{
-//   height: number;
-//   width: number;
-// }>(() => {
-//   let height = refOuterContainer.current?.offsetHeight || 0;
-//   let width = refOuterContainer.current?.offsetWidth || 0;
-
-//   return { height, width };
-// }, [msDataRef.current, cache.visibleItemRange]);
-
-// const innerContainerStyle = useMemo<{
-//   innerMargin: number;
-//   innerSize: number;
-//   totalSize: number;
-//   height: number;
-//   width: number;
-// }>(() => {
-//   let innerMargin = 0;
-//   let totalSize = 0;
-//   let innerSize = 0;
-//   let height = refOuterContainer.current?.offsetHeight || 0;
-//   let width = refOuterContainer.current?.offsetWidth || 0;
-
-//   let lastVisibleItemOffsets =
-//     msDataRef.current[getLastArrItem(cache.visibleItemRange)];
-
-//   if (lastVisibleItemOffsets) {
-//     innerMargin =
-//       msDataRef.current[getFirstArrItem(cache.visibleItemRange)]?.start || 0;
-
-//     totalSize = Math[
-//       getLastArrItem(cache.visibleItemRange) < itemsLength ? 'max' : 'min'
-//     ](
-//       lastVisibleItemOffsets.end, //+ lastVisibleItemOffsets.size, // todo
-//       getLastArrItem(msDataRef.current).end,
-//     );
-//   }
-//   innerSize = totalSize - innerMargin;
-
-//   return { innerMargin, innerSize, totalSize, height, width };
-// }, [msDataRef.current, cache.visibleItemRange]);
-
-// return { outerContainerStyle, innerContainerStyle };
-
-// const [containerStyle, setContainerStyle] = useState<{
-//   outerContainerStyle: {
-//     height: number;
-//     width: number;
-//   };
-//   innerContainerStyle: {
-//     innerMargin: number;
-//     innerSize: number;
-//     totalSize: number;
-//   };
-// }>({
-//   outerContainerStyle: {
-//     height: 0,
-//     width: 0,
-//   },
-//   innerContainerStyle: {
-//     innerMargin: 0,
-//     innerSize: 0,
-//     totalSize: 0,
-//   },
-// });
-
-// useEffect(() => {
-//   let innerMargin = 0;
-//   let totalSize = 0;
-//   let innerSize = 0;
-//   let height = refOuterContainer.current?.offsetHeight || 0;
-//   let width = refOuterContainer.current?.offsetWidth || 0;
-
-//   let lastVisibleItemOffsets =
-//     msDataRef.current[getLastArrItem(cache.visibleItemRange)];
-
-//   if (lastVisibleItemOffsets) {
-//     innerMargin =
-//       msDataRef.current[getFirstArrItem(cache.visibleItemRange)]?.start || 0;
-
-//     totalSize = Math[
-//       getLastArrItem(cache.visibleItemRange) < itemsLength ? 'max' : 'min'
-//     ](
-//       lastVisibleItemOffsets.end, //+ lastVisibleItemOffsets.size, // todo
-//       getLastArrItem(msDataRef.current).end,
-//     );
-//   }
-//   innerSize = totalSize - innerMargin;
-
-//   setContainerStyle({
-//     outerContainerStyle: {
-//       height: height,
-//       width: width,
-//     },
-//     innerContainerStyle: {
-//       innerMargin: innerMargin,
-//       totalSize: totalSize,
-//       innerSize: innerSize,
-//     },
-//   });
-
-//   // return { innerMargin, innerSize, totalSize, height, width };
-// }, [
-//   msDataRef.current,
-//   cache.visibleItemRange,
-//   refOuterContainer.current,
-//   refInnerContainer.current,
-// ]);
-
-// return {
-//   outerContainerStyle: containerStyle.outerContainerStyle,
-//   innerContainerStyle: containerStyle.innerContainerStyle,
-// };
-
-// const [stateStyle, setStateStyle] = useState<{
-//   outerContainerStyle: {
-//     height: number;
-//     width: number;
-//   };
-//   innerContainerStyle: {
-//     innerMargin: number;
-//     innerSize: number;
-//     totalSize: number;
-//     heightInner: number;
-//     widthInner: number;
-//   };
-// }>({
-//   outerContainerStyle: {
-//     height: 0,
-//     width: 0,
-//   },
-//   innerContainerStyle: {
-//     innerMargin: 0,
-//     innerSize: 0,
-//     totalSize: 0,
-//     heightInner: 0,
-//     widthInner: 0,
-//   },
-// });
-
-// const containerStyles = useMemo<{
-//   innerMargin: number;
-//   innerSize: number;
-//   totalSize: number;
-//   height: number;
-//   width: number;
-//   heightInner: number;
-//   widthInner: number;
-// }>(() => {
-//   let innerMargin = 0;
-//   let totalSize = 0;
-//   let innerSize = 0;
-//   let height = refOuterContainer.current?.offsetHeight || 0;
-//   let width = refOuterContainer.current?.offsetWidth || 0;
-//   let heightInner = refInnerContainer.current?.offsetHeight || 0;
-//   let widthInner = refInnerContainer.current?.offsetWidth || 0;
-
-//   console.log('heightInner,widthInner', heightInner, widthInner);
-
-//   let lastVisibleItemOffsets =
-//     msDataRef.current[getLastArrItem(cache.visibleItemRange)];
-
-//   if (lastVisibleItemOffsets) {
-//     innerMargin =
-//       msDataRef.current[getFirstArrItem(cache.visibleItemRange)]?.start || 0;
-
-//     totalSize = Math[
-//       getLastArrItem(cache.visibleItemRange) < itemsLength ? 'max' : 'min'
-//     ](
-//       lastVisibleItemOffsets.end, //+ lastVisibleItemOffsets.size, // todo
-//       getLastArrItem(msDataRef.current).end,
-//     );
-//   }
-//   innerSize = totalSize - innerMargin;
-
-//   setStateStyle({
-//     outerContainerStyle: { height, width },
-//     innerContainerStyle: {
-//       innerMargin,
-//       innerSize,
-//       totalSize,
-//       heightInner,
-//       widthInner,
-//     },
-//   });
-
-//   return {
-//     innerMargin,
-//     innerSize,
-//     totalSize,
-//     height,
-//     width,
-//     heightInner,
-//     widthInner,
-//   };
-// }, [
-//   msDataRef.current,
-//   cache.visibleItemRange,
-//   // refOuterContainer.current,
-//   // refInnerContainer.current?.offsetWidth,
-//   // refInnerContainer.current?.offsetHeight,
-// ]);
-
-// return {
-//   outerContainerStyle: {
-//     height: stateStyle.outerContainerStyle.height,
-//     width: stateStyle.outerContainerStyle.width,
-//   },
-//   innerContainerStyle: {
-//     innerMargin: stateStyle.innerContainerStyle.innerMargin,
-//     totalSize: stateStyle.innerContainerStyle.totalSize,
-//     innerSize: stateStyle.innerContainerStyle.innerSize,
-//     heightInner: stateStyle.innerContainerStyle.heightInner,
-//     widthInner: stateStyle.innerContainerStyle.widthInner,
-//   },
-// };
